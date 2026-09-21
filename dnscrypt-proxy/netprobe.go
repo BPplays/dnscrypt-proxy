dd6bda84f3e66d2a3a1c36c92227f669e7925d57
BPplays <suzuko.oss@proton.me>
2026-09-21 08:54:31 -0700

    merged windows and other; now fully deadline based for single

--- dnscrypt-proxy/netprobe.go
+++ dnscrypt-proxy/netprobe.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"errors"
-	"net/netip"
-	"time"
 	"fmt"
+	"net"
+	"net/netip"
+	"runtime"
+	"time"
 
 	"github.com/jedisct1/dlog"
 )
@@ -106,6 +108,89 @@ func NetProbe(
 			}
 
 		}
+	}
+}
+
+func NetProbeSingle(
+	proxy *Proxy,
+	address netip.AddrPort,
+	ctx context.Context,
+) (ok bool, err error) {
+	if !address.IsValid() || ctx.Err() != nil {
+		return false, nil
+	}
+
+	remoteUDPAddr := net.UDPAddrFromAddrPort(address)
+
+	retried := false
+
+	dialer := net.Dialer{
+		Timeout: proxy.timeout,
+	}
+
+
+	deadline, deadlineOk := ctx.Deadline()
+
+
+	interval := time.Second
+
+	if deadlineOk {
+		if i, _, ok := DeadlineInterval(
+			time.Second,
+			deadline,
+			10 * time.Millisecond,
+		); ok {
+			interval = i
+		}
+	}
+
+	for {
+		startTimer := time.NewTimer(interval)
+
+		pc, err := dialer.DialContext(
+			ctx,
+			"udp",
+			remoteUDPAddr.String(),
+		)
+		if runtime.GOOS == "windows" && err == nil {
+			// Write at least 1 byte. This ensures that sockets are ready to use for writing.
+			// Windows specific: during the system startup, sockets can be created but the underlying buffers may not be
+			// set up yet. If this is the case Write fails with WSAENOBUFS: "An operation on a socket could not be
+			// performed because the system lacked sufficient buffer space or because a queue was full"
+			_, err = pc.Write([]byte{0})
+			if err != nil {
+				pc.Close()
+			}
+		}
+
+		if err != nil {
+			if !retried {
+				retried = true
+				dlog.Noticef(
+					"(%s) Network not available yet -- waiting...",
+					address.String(),
+				)
+			}
+			dlog.Debugf(
+				"(%s) %v",
+				address.String(),
+				err,
+			)
+
+			select {
+			case <-ctx.Done():
+				dlog.Debugf(
+					"(%s) context done",
+					address.String(),
+				)
+				return false, ctx.Err()
+			case <-startTimer.C:
+			}
+
+			continue
+		}
+		pc.Close()
+		return true, nil
 	}
 }
 
